1471a2b13ffae2e7f431f77c83a87bf196acb749
ィスビン <dktnqls0624@naver.com>
2021-07-24 00:39:15 +0900

    secret information

--- index.js
+++ index.js
@@ -1,16 +1,40 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require('body-parser');
+const {User} = require('./models/User');
 
+const config = require('./config/key');
+//application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended:true}));
+//application/json
+app.use(bodyParser.json());
+
+//몽고디비 연결
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://soobeen:asd123@boilerplate.6xx6a.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+mongoose.connect(config.mongoURI, {
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then( ()=> console.log('mongoDB Connected...')).catch(err => console.log(err));
 
 
 app.get('/', (req, res) => {
-  res.send('Hello World!~~ 안녕하세요')
+  res.send('Hello World!~~ 안녕하세요!!!')
 })
+
+
+app.post('/register', (req, res) => {
+  // 회원가입할때 필요한 정보들을  client에서 가져오면
+  // 그것들을 데이터 베이스에 넣어준다.
+  const user = new User(req.body)
+
+  user.save((err, doc) => {
+    if(err) return res.json({success : false , err})
+    return res.status(200).json({
+      success: true
+    })
+  })
+
+});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
